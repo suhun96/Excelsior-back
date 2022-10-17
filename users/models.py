862ad0bfe8f4edde6ec5ecc8ac0367e68a09d71e
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-10-17 18:26:02 +0900

    apply .gitignore

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from email.policy import default
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    phone       = models.ImageField(max_length = 100, blank = False, unique = True)
+    name        = models.CharField(max_length = 50, blank = False)
+    password    = models.CharField(max_length = 50, blank = False)
+    position    = models.CharField(max_length = 50, blank = False)
+    admin       = models.BooleanField(default = False)
+    status      = models.BooleanField(default = True)
+    created_at  = models.DateTimeField(auto_now_add = True)
+    updated_at  = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        db_table = 'users'
