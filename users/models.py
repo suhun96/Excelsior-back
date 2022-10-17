819bf018552a0ca8811114c5be79144a4c34d2d1
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-10-17 19:34:21 +0900

    1017 오류 수정

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     phone       = models.IntegerField(blank = False, unique = True)
-    name        = models.CharField(max_length = 50, blank = False)
+    name        = models.CharField(max_length = 100, blank = False)
     password    = models.CharField(max_length = 50, blank = False)
     position    = models.CharField(max_length = 50, blank = False)
     admin       = models.BooleanField(default = False)
