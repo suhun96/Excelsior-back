4bf327e23983b923070bc66100d99bc83ee4a9e3
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-10-17 18:33:27 +0900

    pymysql 설정

--- excelsior_backend/settings.py
+++ excelsior_backend/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import pymysql
 from my_settings import DATABASES,SECRET_KEY
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+pymysql.install_as_MySQLdb()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
