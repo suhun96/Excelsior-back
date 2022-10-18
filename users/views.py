5ced24278b1d880af2509fa7561172181c59ca6d
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-10-18 10:43:11 +0900

    user model migration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from turtle import position
+from unicodedata import name
+from django.views   import View
+from django.http    import JsonResponse, HttpResponse
+from django.conf    import settings
 
-# Create your views here.
+# Model
+from users.models   import *
+
+# View
+class SignUpView(View):
+    def post(self, request):
+        data = request.POST
+        try:
+            new_user = User.objects.create(
+                phone       = data.phone,
+                name        = data.name,
+                password    = data.phone,
+                position    = data.position,
+                admin       = 0, # True = 관리자 , False = 일반회원
+                status      = 1  # True = 활성화 , False = 비활성화
+            )
+
+            check_user_info = list(User.objects.get(id = new_user.id)) 
+            return JsonResponse({'message' : check_user_info }, status = 200)
+
+        except:
+            return JsonResponse({'message' : 'SignUpView Error'}, status = 403)
