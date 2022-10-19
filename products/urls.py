c95ad61e7ecf371e0f6b87ed0fcaedbcea0d2863
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-10-19 16:58:41 +0900

    10/19 CreateProductGroupView 작성 - 테스트

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 
-from users.views import *
+from products.views import *
 
 urlpatterns = [
-    path('signup', SignUpView.as_view()),
-    path('signin', SignInView.as_view()),
-    path('modify', ModifyView.as_view()),
-    path('pause', ChangeStatusView.as_view()),
-    path('',UserListView.as_view())
+    path('create/pg', CreateProductGroupView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.views       import View
+from django.http        import JsonResponse
+from django.db          import transaction
+from django.db.models   import Q
 
-# Create your views here.
+# Model
+from users.models       import *
+from products.models    import *
+from users.jwtdecoder   import jwt_decoder
+
+class CreateProductGroupView(View):
+    @jwt_decoder
+    def post(self, request):
+        input_data = request.POST
+        user = request.user
+
+        try:
+            if User.objects.get(id = user.id).status == False:
+                return JsonResponse({'message' : 'This account is not available.'})
+
+            new_PG , is_created = ProductGroup.objects.filter(
+                Q(name = input_data['name']) | Q(code = input_data['code'])
+            ).get_or_create(
+                defaults= {
+                    'name' : input_data['name'],
+                    'code' : input_data['code'],
+                    'etc'  : input_data['etc']
+                })
+
+            if is_created == False:
+                return JsonResponse({'messaga' : 'The product name(product code) is already registered.'}, status = 403)      
+
+            check_PG = list(ProductGroup.objects.filter(id = new_PG.id).values(
+                'id',
+                'name',
+                'code',
+                'etc'
+            ))
+
+            return JsonResponse({'message' : check_PG}, status = 200)
+       
+        except KeyError:
+            return JsonResponse({'message' : 'KEY ERROR'}, status = 403)
