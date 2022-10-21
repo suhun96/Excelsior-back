69bbeef6e49b419d46f1d0d0d6c717d58d0ced4a
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-10-21 18:48:15 +0900

    10/21 (금) Product Model 결함 발견 로직 전면 수정! -2

--- products/models.py
+++ products/models.py
@@ -31,7 +31,8 @@ class SerialCode(models.Model):
     company         = models.ForeignKey(Company, on_delete = models.CASCADE)
     model_number    = models.IntegerField()
     code = models.CharField(max_length = 15, blank = False)
-
+    created_at  = models.DateTimeField(auto_now_add = True)
+    
     class Meta:
         db_table = 'serial_code'
 
@@ -44,7 +45,8 @@ class UseStatus(models.Model):
 class Product(models.Model):
     use_status      = models.ForeignKey(UseStatus, on_delete = models.CASCADE)
     serial_code     = models.ForeignKey(SerialCode, on_delete = models.CASCADE)
-    price           = models.BigIntegerField()              
+    price           = models.BigIntegerField()
+    barcode         = models.CharField(max_length = 20, blank = False)              
     etc             = models.CharField(max_length = 3000, blank = True)
     created_at      = models.DateTimeField(auto_now_add = True)
     updated_at      = models.DateTimeField(auto_now = True)
@@ -53,24 +55,32 @@ class Product(models.Model):
         db_table = 'products'
 
 class ProductInfo(models.Model):
-    product_serial_code = models.CharField(max_length = 30, blank = False)
+    serial_code     = models.ForeignKey(SerialCode, on_delete = models.CASCADE)
     quantity        = models.IntegerField()
     safe_quantity   = models.IntegerField()
     search_word     = models.CharField(max_length = 150, blank = False)
     name            = models.CharField(max_length = 15, blank = False)
+    created_at      = models.DateTimeField(auto_now_add = True)
+    updated_at      = models.DateTimeField(auto_now = True)
     
     class Meta:
         db_table = 'product_info'
 
-class WarehousingOrder(models.Model):
-    user    = models.ForeignKey(User, on_delete = models.CASCADE)
-    product = models.ForeignKey(Product, on_delete = models.CASCADE)
-    company = models.ForeignKey(Company, on_delete = models.CASCADE)
+class InboundOrder(models.Model):
+    user            = models.ForeignKey(User, on_delete = models.CASCADE)
+    company         = models.ForeignKey(Company, on_delete = models.CASCADE)
+    etc             = models.CharField(max_length = 3000, blank = True)
+    created_at      = models.DateTimeField(auto_now_add = True)
+    
+    class Meta:
+        db_table = 'inbound_order'
+
+class InboundQuantity(models.Model):
+    inbound_order   = models.ForeignKey(InboundOrder, on_delete = models.CASCADE)
+    serial_code     = models.ForeignKey(SerialCode, on_delete = models.CASCADE)
     inbound_price   = models.BigIntegerField()
     inbound_quntity = models.IntegerField()
-    inbound_address = models.CharField(max_length = 200, blank = False)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
-
+    created_at      = models.DateTimeField(auto_now_add = True)
+    
     class Meta:
-        db_table = 'warehousing_orders'
+        db_table = 'inbound_quantity'
