162962b2eea2aab634b13cbb1eb7d0f98eeccece
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-10-27 18:14:38 +0900

    10/27 (목) utils.py product_history_generator 수정 및 보완 테스트 완료

--- products/views.py
+++ products/views.py
@@ -124,43 +124,43 @@ class CreateProductInfoView(View):
         
         return product_code
 
-    def product_history_generator(self, product_code, quantity, price ,etc):
-        try:
-            # 제품 history에서 사용 가능한
-            product_his = ProductHis.objects.filter(product_code = product_code, use_status = 1).values('product_code')
-            print(product_his)
-            if product_his.exists():
-                before_quantity = product_his.count()
-                print(before_quantity)
+    # def product_history_generator(self, product_code, quantity, price ,etc):
+    #     try:
+    #         # 제품 history에서 사용 가능한
+    #         product_his = ProductHis.objects.filter(product_code = product_code, use_status = 1).values('product_code')
+    #         print(product_his)
+    #         if product_his.exists():
+    #             before_quantity = product_his.count()
+    #             print(before_quantity)
                 
-                for i in range(1 , quantity +1):
-                    zero_num = str(i + before_quantity).zfill(3)
-                    barcode = product_code + zero_num + self.year[2:4] + self.month + self.day
+    #             for i in range(1 , quantity +1):
+    #                 zero_num = str(i + before_quantity).zfill(3)
+    #                 barcode = product_code + zero_num + self.year[2:4] + self.month + self.day
                     
-                    ProductHis.objects.create(
-                        use_status = 1,
-                        product_code = product_code,
-                        price = price,
-                        barcode = barcode,
-                        etc = etc
-                    )
+    #                 ProductHis.objects.create(
+    #                     use_status = 1,
+    #                     product_code = product_code,
+    #                     price = price,
+    #                     barcode = barcode,
+    #                     etc = etc
+    #                 )
 
-                return print('기존 제품을 참고하여 히스토리 생성완료')
-            else:
-                for i in range(1, int(quantity) + 1):
-                    zero_num = str(i).zfill(3)
-                    barcode = product_code + zero_num + self.year[2:4] + self.month + self.day
-                    #SSPP001-221026-001-0001
-                    ProductHis.objects.create(
-                    use_status = 1,
-                    product_code = product_code,
-                    price = price,
-                    barcode = barcode,
-                    etc = etc)
+    #             return print('기존 제품을 참고하여 히스토리 생성완료')
+    #         else:
+    #             for i in range(1, int(quantity) + 1):
+    #                 zero_num = str(i).zfill(3)
+    #                 barcode = product_code + zero_num + self.year[2:4] + self.month + self.day
+    #                 #SSPP001-221026-001-0001
+    #                 ProductHis.objects.create(
+    #                 use_status = 1,
+    #                 product_code = product_code,
+    #                 price = price,
+    #                 barcode = barcode,
+    #                 etc = etc)
 
-                return print('새로운 제품 히스토리 생성완료')
-        except KeyError:
-            return JsonResponse({'message' : '키 에러'}, status = 403)
+    #             return print('새로운 제품 히스토리 생성완료')
+    #     except KeyError:
+    #         return JsonResponse({'message' : '키 에러'}, status = 403)
      
     def post(self, request):
         input_data = request.POST
@@ -180,7 +180,7 @@ class CreateProductInfoView(View):
                 resent_OB_price = 0
                 )
             
-            self.product_history_generator(product_code, input_data['quantity'],input_data['price'] ,input_data['etc'] )
+            product_history_generator(product_code, input_data['quantity'],input_data['price'] ,input_data['etc'] )
 
             return JsonResponse({'mesaage' : '제품 정보가 등록되었습니다.'}, status = 200) 
         except KeyError:
