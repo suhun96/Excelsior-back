63eaa53b490c60ee456bc48af6c9289872516a2e
Ubuntu <ubuntu@ip-172-31-33-146.ap-northeast-2.compute.internal>
2022-11-07 01:33:28 +0000

    wip

--- excelsior_backend/settings.py
+++ excelsior_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '*']
+ALLOWED_HOSTS = ['3.39.18.177', 'yamuzin.net', '.yamuzin.net', '172.31.33.146']
 
 
 # Application definition
