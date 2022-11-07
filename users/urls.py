0afdd8ffbe6e6a3a4d9c8a083706aa6149145b42
Ubuntu <ubuntu@ip-172-31-33-146.ap-northeast-2.compute.internal>
2022-11-07 01:51:45 +0000

    merge main

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,9 @@ from django.urls import path
 from users.views import *
 
 urlpatterns = [
-    path('check', CheckView.as_view()),
     path('signup', SignUpView.as_view()),
     path('signin', SignInView.as_view()),
     path('modify', ModifyView.as_view()),
     path('pause', ChangeStatusView.as_view()),
-    path('',UserInfoView.as_view())
+    path('my',UserInfoView.as_view())
 ]
