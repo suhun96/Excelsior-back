c7cce386b1328259bf00397c201be3c6521cdd21
Ubuntu <ubuntu@ip-172-31-33-146.ap-northeast-2.compute.internal>
2022-11-08 08:27:00 +0000

    wip

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,12 @@ urlpatterns = [
     path('signup', SignUpView.as_view()),
     path('signin', SignInView.as_view()),
     path('modify', ModifyView.as_view()),
+<<<<<<< HEAD
     path('per', PermissionSignUpView.as_view()),
     path('',UserInfoView.as_view()),
     path('check', CheckPasswordView.as_view())
+=======
+    path('pause', ChangeStatusView.as_view()),
+    path('my',UserInfoView.as_view())
+>>>>>>> 0afdd8f (merge main)
 ]
