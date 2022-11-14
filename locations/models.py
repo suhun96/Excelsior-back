f79124edc0a0ecd3e1d9ea694b715f1c657b65fb
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-11-14 11:19:46 +0900

    11월 14일(월) location app 추가

---
+++ locations/models.py
@@ -0,0 +1,13 @@
+from django.db       import models
+from products.models import *
+# Create your models here.
+
+class LocationProduct(models.Model):
+    Location       = models.CharField(max_length=20, blank= False)
+    product_code   = models.CharField(max_length = 10, blank = False)
+    desc           = models.CharField(max_length=20 , blank = False)
+    quantity       = models.IntegerField(default = 0)
+    created_at     = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        db_table = 'location_product'
