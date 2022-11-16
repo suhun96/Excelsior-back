724504e7e6f8854bf816f1028f3aac2255084507
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-11-16 10:38:24 +0900

    11월 16일(수) locations models.py : class 수정 및 추가

--- locations/models.py
+++ locations/models.py
@@ -1,13 +1,36 @@
 from django.db       import models
 from products.models import *
 # Create your models here.
-class Location(models.Model):
-    code = 
+
+class Warehouse(models.Model):
+    name   = models.CharField(max_length = 60, blank = False)
+    code   = models.CharField(max_length = 20, blank = False)
+    type   = models.CharField(max_length = 40, blank = True)
+    way    = models.CharField(max_length = 100, blank = True )
+    etc    = models.CharField(max_length = 1000, blank = True )
+    status = models.BooleanField(default= True)
+    
+    class Meta:
+        db_table = 'warehouse'
+
+class WarehouseProperty(models.Model):
+    contents = models.CharField(max_length = 100, blank = False)
+    status = models.BooleanField(default= True)
+
+    class Meta:
+        db_table = 'warehouse_property'
+
+class WarehouseType(models.Model):
+    contents = models.CharField(max_length = 100, blank = False)
+    status = models.BooleanField(default= True)
+
+    class Meta:
+        db_table = 'warehouse_type'   
 
 class LocationProduct(models.Model):
-    Location_code  = models.CharField(max_length=20, blank= False)
+    warehouse_code = models.CharField(max_length = 20, blank= False)
+    location_desc  = models.CharField(max_length = 80, blank = False)
     product_code   = models.CharField(max_length = 10, blank = False)
-    desc           = models.CharField(max_length=20 , blank = False)
     quantity       = models.IntegerField(default = 0)
     created_at     = models.DateTimeField(auto_now_add = True)
 
