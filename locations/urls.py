b51e9d1c8161af143c7ddbca9a25e73599169680
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-11-16 15:06:47 +0900

    11월 16일(수) locations views.py, urls.py 작성 완료.

--- locations/urls.py
+++ locations/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 from locations.views import *
 
 urlpatterns = [
+    path('property', CreateWarehousePropertyView.as_view()),
+    path('type', CreateWarehouseTypeView.as_view()),
+    path('',WarehouseInfoView.as_view())
 ]
