66df3a114297369948914a232dde5e8d1f6f49ad
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-11-21 16:13:59 +0900

    11월 21일 (월) users views.py : PUT 키값으로 id가 들어왔을 때 패스 되도록

--- companies/views.py
+++ companies/views.py
@@ -99,7 +99,9 @@ class CompanyView(View):
                 update_options = ['name','keyword','represent','biz_no','biz_type','biz_item','phone','fax','email','address_main','address_desc','zip_code']
 
                 for key, value in modify_data.items():
-                    if not key in update_options:
+                    if key == 'id':
+                        pass
+                    elif not key in update_options:
                         return JsonResponse({'message' : f'{key} 존재하지 않는 키값입니다.'}, status = 403)
                     UPDATE_SET.update({ key : value })
                     
@@ -235,7 +237,9 @@ class CompanyPhonebookView(View):
                 update_options = ['name', 'mobile', 'email']
 
                 for key, value in modify_data.items():
-                    if not key in update_options:
+                    if key == 'id':
+                        pass
+                    elif not key in update_options:
                         return JsonResponse({'message' : f'{key} 존재하지 않는 키 값입니다.'}, status = 403)
                     UPDATE_SET.update({ key : value})
 
