351218560bb4df74db5a20a3e2fac6b3970b03c0
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-11-22 17:34:41 +0900

    11월 22일(화) locations views.py WarehouseInfoView 수정 POST(PUT)

--- locations/views.py
+++ locations/views.py
@@ -43,22 +43,20 @@ class WarehouseInfoView(View):
         if not 'code' in input_data:
             return JsonResponse({'message' : '창고 코드를 입력해주세요.'}, status = 403)
 
-        if Warehouse.objects.filter(code = input_data['code']).exists():
-            return JsonResponse({'message' : '창고 코드가 이미 존재합니다.'}, status = 403)
 
-        CREATE_SET = {}
-        CREATE_OPT = ['name', 'code', 'type', 'way', 'etc']
-
-        for key, value in input_data.items():
-            if key in CREATE_OPT:
-                CREATE_SET.update({key : value})
-            else:
-                # return JsonResponse({'message' : '존재하지 않는 키값입니다.'}, status = 403)
-                pass
-        new_warehouse = Warehouse.objects.create(**CREATE_SET)
+        obj, created = Warehouse.objects.update_or_create(
+            name = input_data['name'],
+            code = input_data['code'],
+            defaults={
+                'type' : input_data['type'],
+                'way'  : input_data['way'],
+                'etc' : input_data['etc']
+            })
         
-        
-        return JsonResponse({'message' : '창고 생성'}, status = 200)
+        if created == False:
+            return JsonResponse({'message' : '기존의 창고정보를 수정했습니다.'}, status = 200)
+        else:
+            return JsonResponse({'message' : '새로운 창고정보를 생성했습니다.'}, status = 200)
 
     def get(self, request):
         warehouse_list = list(Warehouse.objects.all().values())
