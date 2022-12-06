2f875e894694abd510c9ef6966a0dfc3986c25c1
Ubuntu <ubuntu@ip-172-31-33-146.ap-northeast-2.compute.internal>
2022-12-06 04:53:59 +0000

    12월 6일(화) urls.py 전체 수정

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from users.views import *
 
 urlpatterns = [
-    path('',UserMyInfoView.as_view()),
+    path('info',UserMyInfoView.as_view()),
     path('signup', SignUpView.as_view()),
     path('signin', SignInView.as_view()),
     path('modify-user', UserModifyView.as_view()),
