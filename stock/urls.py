f43fa9b64a6629b958e3844eb94baa0caf2acdb6
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-12-06 11:38:47 +0900

    12월 5일(금) Stock views.py : NomalStockView post-genetate 완성

--- stock/urls.py
+++ stock/urls.py
@@ -5,6 +5,6 @@ from stock.views import *
 urlpatterns = [
     path('sheet', NomalStockView.as_view()),
     path('list', QunatityByWarehouseView.as_view()),
-    path('sheet-list', SheetListView.as_view()),
-    path('sheet-click', ClickSheetView.as_view())
+    path('sheet-list', SheetListView.as_view()),  # 쿼리 파라미터 type으로 필터링 가능('inbound', 'outbound)
+    path('sheet-click', ClickSheetView.as_view()) #  쿼리 파라미터 sheet_id 필요합니다
 ]
