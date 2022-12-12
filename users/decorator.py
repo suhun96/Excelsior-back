2e452c8eb1161701ee83edd1bd02f01547bccd5e
Ubuntu <ubuntu@ip-172-31-33-146.ap-northeast-2.compute.internal>
2022-12-12 04:27:09 +0000

    check1

--- users/decorator.py
+++ users/decorator.py
@@ -1,7 +1,7 @@
 import jwt
 
 from django.http                import JsonResponse
-from users.models               import User, UserPermission
+from users.models               import User
 from excelsior_backend.settings import SECRET_KEY, ALGORITHM
 
 
@@ -14,11 +14,11 @@ def jwt_decoder(func):
             user        = User.objects.get(id = payload['user_id'])
             
             if user.status == False:
-                return JsonResponse({'message' : f'{user.name}은 허가되지 않은 상태입니다.'}, status = 40)
+                return JsonResponse({'message' : f'{user.name}은 허가되지 않은 상태입니다.'}, status = 403)
             
-            user_permission = UserPermission.objects.get(user_id = user.id)
+            # user_permission = UserPermission.objects.get(user_id = user.id)
 
-            request.user_permission = user_permission
+            # request.user_permission = user_permission
             request.user = user
 
 
