10c4ffc3d1ad995833180075e099babf5382abb5
Ubuntu <ubuntu@ip-172-31-33-146.ap-northeast-2.compute.internal>
2023-01-09 07:46:43 +0000

    merge

--- locustfile.py
+++ locustfile.py
@@ -5,8 +5,12 @@ from locust import HttpUser, task, between
 class WebsiteTestUser(HttpUser):
     wait_time = between(1, 2.5)
 
-    @task
-    def my_task(self):
+    # @task
+    # def my_task(self):
+    #     self.token   = Token
+    #     self.headers = {'Authorization' : self.token}
+    #     self.client.get('/product/info?offset=0&limit=10', headers= self.headers)
+
+    def create_sheet_test(self):
         self.token   = Token
         self.headers = {'Authorization' : self.token}
-        self.client.get('/product/info?offset=0&limit=10', headers= self.headers)
