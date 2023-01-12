7415a4c0be0721e3e278c154e91c7376c6e73082
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2023-01-12 10:42:58 +0900

    1월 12일(목) | SerialCodeCheckView 수정

--- locustfile.py
+++ locustfile.py
@@ -11,46 +11,46 @@ from locust import HttpUser, task, between
 #         self.headers = {'Authorization' : self.token}
 #         self.client.get('/product/info?offset=0&limit=10', headers= self.headers)
 
-class SheetInbound(HttpUser):
-    wait_time = between(1, 2.5)
+# class SheetInbound(HttpUser):
+#     wait_time = between(1, 2.5)
 
-    @task
-    def task_inbound(self):
-        self.token   = Token
-        self.headers = {'Authorization' : self.token}
-        self.client.get('/product/info?offset=0&limit=10', headers= self.headers)
-        self.data = {
-            "type" : "inbound",
-            "date" : "2023-01-09",
-            "company_id": 2,
-            "etc"  : "cLocust - test1",
-            "products" : [
-                {
-                    "product_code": "EXPP001",
-                    "quantity"  : 10,
-                    "location"  : "위치",
-                    "price"     : 30000,
-                    "warehouse_code" : "A",
-                    "etc"       : "Locust - test1"
-                },
-                {
-                    "product_code": "SWVV001",
-                    "quantity"  : 10,
-                    "location"  : "위치",
-                    "price"     : 13000,
-                    "warehouse_code" : "A",
-                    "etc"       : "Locust - test1"
-                },
-                {
-                    "product_code": "BT001",
-                    "quantity"  : 20,
-                    "location"  : "위치",
-                    "price"     : 200,
-                    "warehouse_code" : "A",
-                    "etc"       : "Locust - test1"
-                }
-                ]}
-        self.client.post('/stock/sheet', headers= self.headers, json= self.data)
+#     @task
+#     def task_inbound(self):
+#         self.token   = Token
+#         self.headers = {'Authorization' : self.token}
+#         self.client.get('/product/info?offset=0&limit=10', headers= self.headers)
+#         self.data = {
+#             "type" : "inbound",
+#             "date" : "2023-01-09",
+#             "company_id": 2,
+#             "etc"  : "cLocust - test1",
+#             "products" : [
+#                 {
+#                     "product_code": "EXPP001",
+#                     "quantity"  : 10,
+#                     "location"  : "위치",
+#                     "price"     : 30000,
+#                     "warehouse_code" : "A",
+#                     "etc"       : "Locust - test1"
+#                 },
+#                 {
+#                     "product_code": "SWVV001",
+#                     "quantity"  : 10,
+#                     "location"  : "위치",
+#                     "price"     : 13000,
+#                     "warehouse_code" : "A",
+#                     "etc"       : "Locust - test1"
+#                 },
+#                 {
+#                     "product_code": "BT001",
+#                     "quantity"  : 20,
+#                     "location"  : "위치",
+#                     "price"     : 200,
+#                     "warehouse_code" : "A",
+#                     "etc"       : "Locust - test1"
+#                 }
+#                 ]}
+#         self.client.post('/stock/sheet', headers= self.headers, json= self.data)
 
 
 # class ProductList(HttpUser):
@@ -61,4 +61,12 @@ class SheetInbound(HttpUser):
 #         self.token   = Token
 #         self.headers = {'Authorization' : self.token}
         # self.client.get('/product/info?offset=0&limit=10', headers= self.headers)
+
+class HitSheetinfo(HttpUser):
+    wait_time = between(1, 2.5)
+
+    @task
+    def hit(self):
+        self.client.get('/stock/sheet-info?date_start=2023-01-09&date_end=2023-01-11&offset=0&limit=200')
+
 
