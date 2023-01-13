f88dd6082c0cbd1fa218be63425758701a7089a1
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2023-01-13 16:38:45 +0900

    1월 13일(금) : ModifyMovingAverageMethodView 수정

--- stock/views.py
+++ stock/views.py
@@ -1111,3 +1111,29 @@ class ModifyMovingAverageMethodView(View):
             target.save()
         except Exception:
             return JsonResponse({'message' : '커스텀 가격을 수정하는데 애러가 발생'})
+
+class InquireSheetLogView(View):
+    def get(self, request):
+        sheet_id = request.GET.get('sheet_id')
+        print(sheet_id)
+        
+        log_id_list = SheetLog.objects.filter(sheet_id = sheet_id).values_list('id', flat= True)
+
+        result = list(SheetCompositionLog.objects.filter(sheet_log_id__in = log_id_list).values(
+            'sheet_log__sheet_id',
+            'sheet_log__user_name',
+            'sheet_log__type',
+            'sheet_log__company',
+            'sheet_log__etc',
+            'sheet_log__created_at',
+            'product_id'
+            'product__product_code'
+            'product__name'
+            'unit_price'
+            'quantity',
+            'warehouse_code',
+            'location',
+            'etc'
+        ))
+        return JsonResponse({'message' : result}, status = 200)
+            
