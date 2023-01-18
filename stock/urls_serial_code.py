4d7fc942ee8ca2d99d841a26f69699d12e5b9629
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2023-01-18 10:18:00 +0900

    1월 18일(수): DeleteSerialCodeValueView 생성

--- stock/urls_serial_code.py
+++ stock/urls_serial_code.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('title-modify', ModifySerialCodeTitleView.as_view()), # POST / Form-data
     path('title-list',   InquireSerialCodeTitleView.as_view()), # GET / Query-parameter
     path('value-create', CreateSerialCodeValueView.as_view()), # POST / Form-data
+    path('value-delete', DeleteSerialCodeValueView.as_view()),
     path('check', SerialCodeCheckView.as_view()),
     path('code-list', InquireSerialCodeView.as_view()),
     path('set-code-list', InquireSetSerialCodeView.as_view()),
